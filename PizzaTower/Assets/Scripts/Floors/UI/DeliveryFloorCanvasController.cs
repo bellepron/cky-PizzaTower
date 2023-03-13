4a1f991a749a201da8b4ec2260c354780ff546dd
Çağlar Kutlu Yiğit <caglarkutluyigit@gmail.com>
2023-03-13 18:33:31 +0300

    refactor: delivery floor canvas

--- PizzaTower/Assets/Scripts/Floors/UI/DeliveryFloorCanvasController.cs
+++ PizzaTower/Assets/Scripts/Floors/UI/DeliveryFloorCanvasController.cs
@@ -1,9 +1,19 @@
 using PizzaTower.Managers;
+using UnityEngine;
 
 namespace PizzaTower.Floors.UI
 {
     public class DeliveryFloorCanvasController : FloorCanvasControllerAbstract
     {
+        [SerializeField] private GameObject buttonHolder;
+        private DeliveryFloorSettings _deliveryFloorSettings;
+
+        protected override void Start()
+        {
+            base.Start();
+            _deliveryFloorSettings = LevelManager.Instance.levelSettings.DeliveryFloorSettings;
+        }
+
         protected override void GetCosts()
         {
             costs = LevelManager.Instance.levelSettings.DeliveryFloorSettings.DeliveryFloorCosts;
@@ -14,6 +24,19 @@ namespace PizzaTower.Floors.UI
             base.SubscribeEvents();
 
             EventManager.DeliveryFloorUpgrade += OnUpgrade;
+        }
+
+        protected override void OnUpgrade(int level)
+        {
+            if (level == _deliveryFloorSettings.DeliveryMaxLevel)
+            {
+                EventManager.DeliveryFloorUpgrade -= OnUpgrade;
+                buttonHolder.SetActive(false);
+
+                return;
+            }
+
+            base.OnUpgrade(level);
         }
     }
 }
